e1c817adc9e65283fe1ab796ab0263ce7400c1c2
SARA1993 <>
2021-06-03 12:10:11 +0000

    sa

---
+++ javascipt.js
@@ -0,0 +1,14 @@
+alert("Welcom To Petra Website")
+
+var name;
+name="sara"; 
+
+document.write("<p style='color:white;'> this is " + name + " from js </p>")
+
+var usercountry = prompt(" Where Are You From?")
+
+if (usercountry === "jordan"){alert ("welcom")}
+else {alert ("WElcom To Jordan")}
+
+
+
